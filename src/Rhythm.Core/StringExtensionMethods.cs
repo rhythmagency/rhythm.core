368ed56d99a4a2d1f4ef2818eb06043ecb9435af
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-11-15 09:46:19 -0800

    Added some extension methods to convert to snake case and to convert to a CSS class. Incremented version number to 1.0.1.0

--- src/Rhythm.Core/StringExtensionMethods.cs
+++ src/Rhythm.Core/StringExtensionMethods.cs
@@ -13,9 +13,30 @@
     public static class StringExtensionMethods
     {
 
+        #region Constants
+
+        private const string SnakeCaseReplace = @"-${EDGE}";
+        private const string CssReplace = @"-";
+
+        #endregion
+
         #region Properties
 
+        /// <summary>
+        /// This regular expression matches the substrings that should have a dash inserted
+        /// before them.
+        /// </summary>
+        private static Regex SlugInsertionPointRegex { get; set; }
+
+        /// <summary>
+        /// This regular expression matches a line (i.e., everything except for line breaks).
+        /// </summary>
         private static Regex LineRegex { get; set; }
+
+        /// <summary>
+        /// This regular expression matches the invalid CSS characters.
+        /// </summary>
+        private static Regex InvalidCssChars { get; set; }
 
         #endregion
 
@@ -27,7 +48,10 @@
         static StringExtensionMethods()
         {
             var options = RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase;
+            var options2 = RegexOptions.Compiled | RegexOptions.Singleline;
             LineRegex = new Regex(@"((?!\r|\n).)+", options);
+            SlugInsertionPointRegex = new Regex(@"(?<EDGE>(?<!^[A-Z]*|-)[A-Z]+)", options2);
+            InvalidCssChars = new Regex(@"((?![a-z0-9]).)+", options);
         }
 
         #endregion
@@ -73,6 +97,43 @@
                     return new[] { source };
             }
 
+        }
+
+        /// <summary>
+        /// Converts a camel case string to snake case (e.g., converts "thisThing" to "this-thing").
+        /// </summary>
+        /// <param name="source">
+        /// The camel case string to convert.
+        /// </param>
+        /// <returns>
+        /// The snake case string.
+        /// </returns>
+        public static string ToSnakeCase(this string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return null;
+            }
+            return SlugInsertionPointRegex.Replace(source, SnakeCaseReplace).ToLower();
+        }
+
+        /// <summary>
+        /// Sanitizes a string for use as a CSS class (e.g., converts
+        /// "some randomText" to "some-random-text").
+        /// </summary>
+        /// <param name="source">
+        /// The string to sanitize.
+        /// </param>
+        /// <returns>
+        /// The string that can be used as a CSS class.
+        /// </returns>
+        public static string SanitizeForCss(this string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return null;
+            }
+            return InvalidCssChars.Replace(source, CssReplace).ToSnakeCase();
         }
 
         #endregion
