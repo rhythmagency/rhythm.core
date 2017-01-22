1eeca363004dee44a1f53e4294087884f100faa5
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-01-21 21:32:12 -0800

    Ensure NuGet packages using the current build configuration (i.e., Debug or Release). Incremented version number to 1.1.1.0.

--- build/nuget.build.bat
+++ build/nuget.build.bat
@@ -3,6 +3,7 @@ REM It is run by the Visual Studio buld process.
 
 SET NUGET=..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe
 SET VER=%1
+SET CONFIG=%2
 
 @ECHO ====== Creating NuGet Package to %OUTDIR% from %cd%
-%NUGET% pack Rhythm.Core.csproj -outputdirectory ..\..\dist -version %VER%
+%NUGET% pack Rhythm.Core.csproj -outputdirectory ..\..\dist -version %VER% -Properties Configuration=%CONFIG%
