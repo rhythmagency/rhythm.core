ca27687ca25b185a4e66f22ff4deb0d769132f2e
Jamie Pollock <jamie.does.umbraco@gmail.com>
2023-05-12 13:53:58 -0400

    Updated StringSplitDelimiters to use file-scoped namespace

--- src/Rhythm.Core/Enums/StringSplitDelimiters.cs
+++ src/Rhythm.Core/Enums/StringSplitDelimiters.cs
@@ -1,51 +1,48 @@
-﻿namespace Rhythm.Core.Enums
+﻿namespace Rhythm.Core.Enums;
+
+/// <summary>
+/// The types of delimiters that can be used to split strings.
+/// </summary>
+public enum StringSplitDelimiters
 {
 
     /// <summary>
-    /// The types of delimiters that can be used to split strings.
+    /// Default will split by common delimiters (e.g., commas, line breaks, semicolons).
     /// </summary>
-    public enum StringSplitDelimiters
-    {
+    Default,
 
-        /// <summary>
-        /// Default will split by common delimiters (e.g., commas, line breaks, semicolons).
-        /// </summary>
-        Default,
+    /// <summary>
+    /// Split by line breaks.
+    /// </summary>
+    LineBreak,
 
-        /// <summary>
-        /// Split by line breaks.
-        /// </summary>
-        LineBreak,
+    /// <summary>
+    /// Split by commas.
+    /// </summary>
+    Comma,
 
-        /// <summary>
-        /// Split by commas.
-        /// </summary>
-        Comma,
+    /// <summary>
+    /// Split by semicolon.
+    /// </summary>
+    Semicolon,
 
-        /// <summary>
-        /// Split by semicolon.
-        /// </summary>
-        Semicolon,
+    /// <summary>
+    /// Split by tabs.
+    /// </summary>
+    Tab,
 
-        /// <summary>
-        /// Split by tabs.
-        /// </summary>
-        Tab,
+    /// <summary>
+    /// Split by equals signs.
+    /// </summary>
+    Equals,
 
-        /// <summary>
-        /// Split by equals signs.
-        /// </summary>
-        Equals,
+    /// <summary>
+    /// Split by \.
+    /// </summary>
+    BackSlash,
 
-        /// <summary>
-        /// Split by \.
-        /// </summary>
-        BackSlash,
-
-        /// <summary>
-        /// Split by /.
-        /// </summary>
-        ForwardSlash,
-    }
-
+    /// <summary>
+    /// Split by /.
+    /// </summary>
+    ForwardSlash,
 }
